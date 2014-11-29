5b9465407407bb19159485540b49cd8fdcc2e514
Carl Tablante <carl.q.tablante@gmail.com>
2014-11-29 17:40:13 +0800

    error debugging

--- tabs.rb
+++ tabs.rb
@@ -2,13 +2,32 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'sanitize'
+require 'pry'
 
-url = ARGV[0]
-filename = ARGV[1]
+song_title = ARGV[0].gsub(' ','+')
 
-doc = Nokogiri::HTML open(url)
-tabs = doc.css('.tb_ct pre')[2].to_s
+url = "http://www.ultimate-guitar.com/search.php?search_type=title&value=#{song_title}"
+page = Nokogiri::HTML open(url, 'User-Agent' => 'chrome')
+results = page.css('.tresults').first.css('tr')
 
-File.open("#{ARGV[1]}.txt", 'w') do |file|
-  file.puts Sanitize.fragment(tabs)
+match = results.select do |doc|
+  begin
+    doc.css('.song').attributes["class"].value == "song"
+  rescue NoMethodError
+  end
 end
+
+binding.pry
+
+# tabs = doc.css('.tb_ct pre')[2].to_s
+
+# File.open("#{ARGV[1]}.txt", 'w') do |file|
+#   file.puts Sanitize.fragment(tabs)
+# end
+
+
+# match criteria
+# 1. if it matches with the title name
+# 2. if it's either a chord or a tab
+# 3. if it's the highest rating
+# 4. if it has the most number of upvotes
